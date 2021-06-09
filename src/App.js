d2f548e64279832b441fafe731be28305fb082a5
Wassim Ben Taleb <wassimwbt@gmail.com>
2021-06-10 00:13:34 +0100

    add react router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React from 'react';
-import HomeManagement from './views/HomeManagement/HomeManagement';
-import ReservationManagement from './views/ReservationManagement/ReservationManagement';
+import { BrowserRouter } from 'react-router-dom'
 import MainLayout from './layout/MainLayout.js'
 import 'antd/dist/antd.css';
 import './App.css';
 function App(props) {
   return (
-    <div>
-      {/* <HomeManagement />
-      <ReservationManagement /> */}
+    <BrowserRouter>
       <MainLayout />
-    </div>
+    </BrowserRouter>
   )
 }
 export default App;
---
+++ src/views/About/About.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Card, Row, Col } from 'antd';
+const { Meta } = Card;
+function About(props) {
+    return (
+        <div>
+            <h2>Vacation Rental v1</h2>
+            <p>
+                This awsome website is created by :
+      </p>
+            <Row justify="space-around" align="middle">
+                <Col span={4}>
+                    <Card
+                        hoverable
+                        style={{ width: 240 }}
+                        cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+                    >
+                        <Meta title="Europe Street beat" description="www.instagram.com" />
+                    </Card>
+                </Col>
+                <Col span={4}>
+                    <Card
+                        hoverable
+                        style={{ width: 240 }}
+                        cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+                    >
+                        <Meta title="Europe Street beat" description="www.instagram.com" />
+                    </Card>
+                </Col>
+            </Row>
+        </div>
+    )
+}
+export default About
