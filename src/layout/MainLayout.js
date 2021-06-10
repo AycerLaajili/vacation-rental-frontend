82a61e712bb5101377056612fdabc15e9638e652
Wassim Ben Taleb <wassimwbt@gmail.com>
2021-06-10 12:18:27 +0100

    working on about page

--- src/layout/MainLayout.js
+++ src/layout/MainLayout.js
@@ -1,5 +1,5 @@
 import { Layout, Menu } from 'antd';
-import { BrowserRouter, Switch, Route, Link } from 'react-router-dom'
+import { Switch, Route, Link } from 'react-router-dom'
 
 import HomeManagement from '../views/HomeManagement/HomeManagement.js'
 import ReservationManagement from '../views/ReservationManagement/ReservationManagement.js'
@@ -9,36 +9,36 @@ function MainLayout(props) {
     const { Header, Content, Footer } = Layout
 
     return (
-        <BrowserRouter>
-            <Layout className="layout">
-                <Header>
-                    <div className="logo" />
-                    <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
-                        <Menu.Item key="1">
-                            <Link to='/'>Home Management</Link>
-                        </Menu.Item>
-                        <Menu.Item key="2">
-                            <Link to='/reservations'>Reservation Management</Link>
-                        </Menu.Item>
-                        <Menu.Item key="3">
-                            <Link to='/about'>About</Link>
-                        </Menu.Item>
-                    </Menu>
-                </Header>
-                <Content style={{ padding: '0 50px' }}>
-                    <br />
-                    <br />
-                    <div className="site-layout-content">
-                        <Switch>
-                            <Route path='/' component={HomeManagement} exact />
-                            <Route path='/reservations' component={ReservationManagement} />
-                            <Route path='/about' component={About} />
-                        </Switch>
-                    </div>
-                </Content>
-                <Footer style={{ textAlign: 'center' }}>Vacation Rental ©2021 Created by Aycer & Wassim</Footer>
-            </Layout>
-        </BrowserRouter>
+
+        <Layout className="layout">
+            <Header>
+                <div className="logo" />
+                <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
+                    <Menu.Item key="1">
+                        <Link to='/'>Home Management</Link>
+                    </Menu.Item>
+                    <Menu.Item key="2">
+                        <Link to='/reservations'>Reservation Management</Link>
+                    </Menu.Item>
+                    <Menu.Item key="3">
+                        <Link to='/about'>About</Link>
+                    </Menu.Item>
+                </Menu>
+            </Header>
+            <Content style={{ padding: '0 50px' }}>
+                <br />
+                <br />
+                <div className="site-layout-content">
+                    <Switch>
+                        <Route path='/' component={HomeManagement} exact />
+                        <Route path='/reservations' component={ReservationManagement} />
+                        <Route path='/about' component={About} />
+                    </Switch>
+                </div>
+            </Content>
+            <Footer style={{ textAlign: 'center' }}>Vacation Rental ©2021 Created by Aycer & Wassim</Footer>
+        </Layout>
+
     )
 }
 
--- src/views/About/About.js
+++ src/views/About/About.js
@@ -1,32 +1,47 @@
 import React from 'react'
-import { Card, Row, Col } from 'antd';
+import { Card, Row, Col, Typography } from 'antd';
+
+import wassimImage from '../../assets/images/wes-info.png'
+import aycerImage from '../../assets/images/aycer.png'
+
+const { Meta } = Card;
+const { Title, Text } = Typography;
 
 function About(props) {
-    const { Meta } = Card;
-
     return (
         <div>
-            <h2>Vacation Rental v1</h2>
-            <p>
-                This awsome website is created by :
-            </p>
+
+            <Typography>
+                <Title>Vacation Rental</Title>
+                <Text>
+                    In the context of GoMyCode Lab Phase, we have created a fully functional web application using
+                </Text>
+                <br />
+                <Text>
+                    the MERN stack that will integrate all the knowledge and skills acquired during the amazing training.
+                </Text>
+            </Typography>
+
+            <br />
+            <br />
+
             <Row justify="space-around" align="middle">
                 <Col span={4}>
                     <Card
                         hoverable
                         style={{ width: 240 }}
-                        cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+                        cover={<img alt="example" src={aycerImage} />}
                     >
-                        <Meta title="Europe Street beat" description="www.instagram.com" />
+                        <Meta title="Aycer Lajili" description="github.com/AycerLaajili" />
                     </Card>
                 </Col>
                 <Col span={4}>
                     <Card
                         hoverable
                         style={{ width: 240 }}
-                        cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+                        cover={<img alt="example" src={wassimImage} />}
                     >
-                        <Meta title="Europe Street beat" description="www.instagram.com" />
+                        <Meta title="Wassim Ben Taleb" description="github.com/Wassim-7" />
                     </Card>
                 </Col>
             </Row>
